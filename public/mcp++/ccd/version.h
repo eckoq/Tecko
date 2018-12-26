d276962a3bef8acf8909360589b53f8f7f460182
eckoqzhang <374660267@qq.com>
2018-12-26 12:14:13 +0800

    delete mcp++

--- public/mcp++/ccd/version.h
+++
@@ -1,6 +0,0 @@
-//版本描述字符串
-extern char version_string[];
-//代码编译日期
-extern char compiling_date[];
-
-extern int get_version_num();
--- public/mcp++/example/echo_server/tools/op/clean_ipc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SHM_IDS=`ipcs -m | awk '{if (match($1, /0x1122337/)) print $1}'`
-
-for SHM_ID in $SHM_IDS; do
-    echo "ipcrm -M $SHM_ID"
-    ipcrm -M $SHM_ID 
-done
